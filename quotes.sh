5b405adc3872861741e53d88a61cc190f15e6ef9
lovincyrus <hello@lovincyrus.com>
2017-06-01 13:19:45 -0700

    Indentation sucks

--- quotes.sh
+++ quotes.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
+#
+# File: quotes.sh
+# Author: Cyrus Goh
+# Last revised: 13:16 June 1, 2017
+
+
 
 IFS="/"
 while read quote source
 do
-		echo "$quote — $source"
+	echo "$quote — $source"
 done < quotes | gshuf -n 1
